eb173b7e9f57894f009c2e2de64fe7d31e85a737
Mrick343 <mrinmoy8429de@gmail.com>
2024-05-16 19:16:33 +0530

    support:  Rebrand to euclidOS

---
+++ src/com/geometrics/support/colorpicker/SecureSettingColorPickerPreference.java
@@ -0,0 +1,26 @@
+package com.geometrics.support.colorpicker;
+
+import android.content.Context;
+import android.text.TextUtils;
+import android.util.AttributeSet;
+import android.provider.Settings;
+
+import com.geometrics.support.preferences.SecureSettingsStore;
+import com.geometrics.support.colorpicker.ColorPickerPreference;
+public class SecureSettingColorPickerPreference extends ColorPickerPreference {
+
+    public SecureSettingColorPickerPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        setPreferenceDataStore(new SecureSettingsStore(context.getContentResolver()));
+    }
+
+    public SecureSettingColorPickerPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setPreferenceDataStore(new SecureSettingsStore(context.getContentResolver()));
+    }
+
+    public SecureSettingColorPickerPreference(Context context) {
+        super(context, null);
+        setPreferenceDataStore(new SecureSettingsStore(context.getContentResolver()));
+    }
+}
